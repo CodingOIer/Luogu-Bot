3c742929e4734d75754693241159ec41a744f281
CodingOIer <wanghongtiancodingoier@outlook.com>
2024-04-08 16:50:39 +0800

    fix main.py

--- main.py
+++ main.py
@@ -288,7 +288,6 @@ def getCsrfToken():
     global cookie
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE',
-        '_contentOnly': 'WoXiHuanFanQianXing',
         'x-luogu-type': 'content-only',
         'cookie': cookie,
         'x-requested-with': 'XMLHttpRequest',
